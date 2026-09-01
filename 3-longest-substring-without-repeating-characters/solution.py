cac59057cf15cffd0c125180ebc1065e0d9c74e0
Haritha N <haritha.neelisetty@gmail.com>
2026-09-01 14:49:11 +0530

    Time: 179 ms | Memory: 19.9 MB - LeetPush

--- 3-longest-substring-without-repeating-characters/solution.py
+++ 3-longest-substring-without-repeating-characters/solution.py
@@ -1,6 +1,6 @@
 # Longest Substring Without Repeating Characters
 # Difficulty: Medium
-# Runtime: 188 ms
+# Runtime: 179 ms
 # Memory: 19.9 MB
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
