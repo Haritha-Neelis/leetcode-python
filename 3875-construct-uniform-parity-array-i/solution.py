b318923bec487613b000bd40e5ad5b36134d20b3
Haritha N <haritha.neelisetty@gmail.com>
2026-09-02 13:24:00 +0530

    Time: 3 ms | Memory: 19.2 MB - LeetPush

---
+++ 3875-construct-uniform-parity-array-i/solution.py
@@ -0,0 +1,10 @@
+# Construct Uniform Parity Array I
+# Difficulty: Easy
+# Runtime: 3 ms
+# Memory: 19.2 MB
+# https://leetcode.com/problems/construct-uniform-parity-array-i/
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
+        
